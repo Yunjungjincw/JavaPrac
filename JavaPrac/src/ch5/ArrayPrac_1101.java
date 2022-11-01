5f44775e09cc245f55163cd40826d72d77a7ae58
yunjungjin <qwe3776@gmail.com>
2022-11-01 11:14:50 +0900

    javaprac 22_11_01 11:14

---
+++ JavaPrac/src/ch5/ArrayPrac_1101.java
@@ -0,0 +1,16 @@
+package ch5;
+
+public class ArrayPrac_1101 {
+
+	public static void main(String[] args) {
+		String[] strArr = new String[5];
+		
+	
+		for(int i = 0; i<strArr.length; i++) {
+			System.out.println(strArr[i]);
+		}
+	
+
+	}
+
+}
