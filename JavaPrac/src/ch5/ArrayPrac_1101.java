13c1d3930ed091d22c2f40fdfb3a9b6ba6464468
yunjungjin <qwe3776@gmail.com>
2022-11-01 11:27:18 +0900

    제발제발

--- JavaPrac/src/ch5/ArrayPrac_1101.java
+++ JavaPrac/src/ch5/ArrayPrac_1101.java
@@ -3,14 +3,10 @@ package ch5;
 public class ArrayPrac_1101 {
 
 	public static void main(String[] args) {
-		String[] strArr = new String[5];
-		
-	
-		for(int i = 0; i<strArr.length; i++) {
-			System.out.println(strArr[i]);
+
+		String[] Sibal = new String[] {"개","고양이","사람"};
+		System.out.println(Sibal.length);
 		}
 	
-
 	}
 
-}
