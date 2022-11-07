8b42a44a6205411c8a962e9f5111aace036a7181
farmer <farmer@DESKTOP-U63O9IM>
2022-11-08 00:45:00 +0900

    자바연습 22_11_08 00:44

---
+++ JavaPrac/src/ch06/sec10/exam01/CalculatorExample.java
@@ -0,0 +1,27 @@
+package ch06.sec10.exam01;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+//		Calculator myCalcu = new Calculator();
+//		double result1 = 10*10*myCalcu.pi;
+//		int result2 = myCalcu.plus(10, 5);
+//		int result3 = myCalcu.minus(10,5);
+		//The static method plus(int, int) from the type Calculator should be accessed in a static way
+		//Calculator 유형의 정적 메서드 plus(int, int)는 정적 방식으로 액세스해야 합니다.
+ 		
+		
+		
+		
+		
+		double result1 = 10 * 10 * Calculator.pi;
+		int result2 = Calculator.plus(10, 5);
+		int result3 = Calculator.minus(10,  5);
+//		
+		
+		System.out.println("result1 : " + result1);
+		System.out.println("result2 : "+ result2);
+
+	}
+
+}
