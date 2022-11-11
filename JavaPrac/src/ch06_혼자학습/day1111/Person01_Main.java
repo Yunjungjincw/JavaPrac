4c29bcb57c279491649b2e454748d6c6a0c9b8c3
user <user@DESKTOP-7RENI2S>
2022-11-11 17:47:40 +0900

    자바연습 22_11_11 17:50

--- JavaPrac/src/ch06_혼자학습/day1111/Person01_Main.java
+++ JavaPrac/src/ch06_혼자학습/day1111/Person01_Main.java
@@ -5,11 +5,11 @@ package ch06_혼자학습.day1111;
 public class Person01_Main {
 
 	public static void main(String[] args) {
-		Person01 p = new Person01();
+//		Person01 p = new Person01();
 		//Cannot instantiate the type Person01
 		//Person01 클래스의 객체를 생성하지 못한다.
 		// 왜냐하면 new 연산자로 객체 생성하지 못하고 상속 통해 자식 클래스만 생성 가능
-		System.out.println(p);
+//		System.out.println(p);
 		
 		//Person01을 상속받는 Student01 객체생성
 		Student01 s = new Student01();
